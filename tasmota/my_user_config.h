90afccc06ab913a367a323dc7c80d946cf9c4dc6
Lihong Hao <hlhcom@gmail.com>
2023-02-13 14:55:31 +0000

    add wifi inofo

--- tasmota/my_user_config.h
+++ tasmota/my_user_config.h
@@ -71,10 +71,10 @@
 #define WIFI_DNS               "192.168.1.254"     // [IpAddress4] If not using DHCP set DNS1 IP address (might be equal to WIFI_GATEWAY)
 #define WIFI_DNS2              "0.0.0.0"         // [IpAddress5] If not using DHCP set DNS2 IP address (might be equal to WIFI_GATEWAY)
 
-#define STA_SSID1              "BTHub6-K3J8"                // [Ssid1] Wi-Fi SSID
+#define STA_SSID1              "BTHub6"                // [Ssid1] Wi-Fi SSID
 #define STA_PASS1              "22345678"                // [Password1] Wi-Fi password
-#define STA_SSID2              ""                // [Ssid2] Optional alternate AP Wi-Fi SSID
-#define STA_PASS2              ""                // [Password2] Optional alternate AP Wi-Fi password
+#define STA_SSID2              "Tasmota"                // [Ssid2] Optional alternate AP Wi-Fi SSID
+#define STA_PASS2              "98765432"                // [Password2] Optional alternate AP Wi-Fi password
 #define WIFI_AP_PASSPHRASE     ""                // AccessPoint passphrase. For WPA2 min 8 char, for open use "" (max 63 char).
 #define WIFI_CONFIG_TOOL       WIFI_RETRY        // [WifiConfig] Default tool if Wi-Fi fails to connect (default option: 4 - WIFI_RETRY)
                                                  // (WIFI_RESTART, WIFI_MANAGER, WIFI_RETRY, WIFI_WAIT, WIFI_SERIAL, WIFI_MANAGER_RESET_ONLY)
